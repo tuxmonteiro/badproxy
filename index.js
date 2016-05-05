8875412dac710155bc6b2eeb91c9f75e5754de43
Igor Fernandes <igor.fernandes@corp.globo.com>
2016-05-05 11:24:37 -0300

    Remove all poisons

--- index.js
+++ index.js
@@ -14,31 +14,31 @@ throng({ lifetime: Infinity }, (id) => {
     //  .rule(rules.probability(50))
     //  .poison(poisons.slowOpen({ delay: 500 }))
 
-    var route = proxy.get('/*')
+    // var route = proxy.get('/*')
 
     //route
     //  .poison(poisons.latency({ jitter: 1000 }))
 
-    route
-      .poison(poisons.inject({ code: 502, body: 'Error!', headers: { 'X-Toxy-Poison': 'error' } }))
-      .withRule(rules.probability(5))
+    // route
+    //   .poison(poisons.inject({ code: 502, body: 'Error!', headers: { 'X-Toxy-Poison': 'error' } }))
+    //   .withRule(rules.probability(5))
 
-    route
-      .poison(poisons.slowClose({ delay: 1000 }))
-      .withRule(rules.probability(5))
+    // route
+    //   .poison(poisons.slowClose({ delay: 1000 }))
+    //   .withRule(rules.probability(5))
 
     //route
     //  .poison(poisons.rateLimit({ limit: 2, threshold: 5000 }))
     //  .withRule(rules.probability(20))
 
-    route
-      .poison(poisons.slowRead({ bps: 100 }))
-      .withRule(rules.probability(5))
+    // route
+    //   .poison(poisons.slowRead({ bps: 100 }))
+    //   .withRule(rules.probability(5))
 
-    route
-      .poison(poisons.abort())
-      .poisonRule(rules.probability(5)) // does the same as withRule()
-      .poisonRule(rules.method('GET'))
+    // route
+    //   .poison(poisons.abort())
+    //   .poisonRule(rules.probability(5)) // does the same as withRule()
+    //   .poisonRule(rules.method('GET'))
 
     proxy.listen(process.env.PORT || 3000)
     console.log(`BadProxy UP (process ${id})`)
